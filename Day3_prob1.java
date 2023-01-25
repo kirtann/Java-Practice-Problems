1c97d8c231fa60d4c36523d2ff0e63fb0de9cd5f
Kirtan Jain <94744255+kirtann@users.noreply.github.com>
2023-01-25 19:43:55 +0530

    Create Day3_prob1.java

---
+++ Day3_prob1.java
@@ -0,0 +1,55 @@
+/*
+Peter asked his friend Max to implement a Java program in which 3 integers X, Y and N are given as a runtime input and all the numbers between X and Y which are divisible by N must be displayed on console. If no such number is possible for the given input then, print NO OUTPUT.
+
+Input Format
+
+3 space separated integers X, Y and N
+
+Constraints
+
+X, Y and N are less than 1000
+
+X and Y are positive integers and X < Y
+
+Output Format
+
+All the integers between X and Y separated by space which are divisible by N
+
+Sample Input 0
+
+6 11 3
+Sample Output 0
+
+6 9
+Sample Input 1
+
+2 6 2
+Sample Output 1
+
+2 4 6
+*/
+
+// kirtan jain
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt(), flag=0;
+        int y = sc.nextInt();
+        int n = sc.nextInt();
+        for(int i=x; i<=y; i++)
+        {
+            if(i%n==0){
+                flag=1;
+                System.out.print(i + " ");
+            }
+            
+        }
+        if(flag==0)
+                System.out.print("NO OUTPUT");
+    }
+}
