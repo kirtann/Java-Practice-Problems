81aa60a90f53ebe34df4423774ae14344a569692
Kirtan Jain <94744255+kirtann@users.noreply.github.com>
2023-01-25 19:36:59 +0530

    Update Day1_prob1.java

--- Day1_prob1.java
+++ Day1_prob1.java
@@ -1,3 +1,35 @@
+/*
+Write a program to take the 2 inputs of integer type from the user. Display the values separated by symbol ‘-’ if the values are positive otherwise display the message “Invalid Input”.
+
+Input Format
+
+Your program should take at least 2 inputs of integer type.
+
+Constraints
+
+Input should be positive integers
+
+Output Format
+
+Output should be the input values separated by ‘-‘ or “Invalid Input” message if any input is negative.
+
+Sample Input 0
+
+10
+2
+Sample Output 0
+
+10-2
+Sample Input 1
+
+20
+-10
+Sample Output 1
+
+Invalid Input
+*/
+
+
 // Kirtan Jain
 // Display of two positive user taken number inputs with '-' sign in between. 
 import java.io.*;
