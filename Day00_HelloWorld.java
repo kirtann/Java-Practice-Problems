c30049d18a3d7ca82fc4c8b05db233dc0c331cf5
Kirtan Jain <94744255+kirtann@users.noreply.github.com>
2023-03-24 19:23:12 +0530

    Create Day00_HelloWorld.java

---
+++ Day00_HelloWorld.java
@@ -0,0 +1,10 @@
+/*
+display "Hello, World!" in java.
+*/
+
+// Kirtan Jain
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
