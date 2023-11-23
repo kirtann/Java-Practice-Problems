6078849a5e70c0b7bf40964ab6b691b989a5774d
Kirtan Jain <94744255+kirtann@users.noreply.github.com>
2023-11-24 00:00:16 +0530

    Update Day00_HelloWorld.java

--- Day00_HelloWorld.java
+++ Day00_HelloWorld.java
@@ -2,7 +2,7 @@
 display "Hello, World!" in java.
 */
 
-// Kirtan Jain
+// Kirtan Jain  
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
